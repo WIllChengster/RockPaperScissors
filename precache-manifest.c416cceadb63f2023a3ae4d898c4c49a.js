0cfd4737399c54ed9bc14d840433902d7c9c6206
William Cheng <willchengsters@gmail.com>
2020-09-11 23:24:12 -0700

    Updates

---
+++ precache-manifest.c416cceadb63f2023a3ae4d898c4c49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c56ff92f2dc41685f67a773d40bbff",
+    "url": "/RockPaperScissors/index.html"
+  },
+  {
+    "revision": "a52690bbc8a46870c60c",
+    "url": "/RockPaperScissors/static/css/main.1573046d.chunk.css"
+  },
+  {
+    "revision": "8b76957b5083fbd20bbe",
+    "url": "/RockPaperScissors/static/js/2.0ba5f672.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RockPaperScissors/static/js/2.0ba5f672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52690bbc8a46870c60c",
+    "url": "/RockPaperScissors/static/js/main.436a5719.chunk.js"
+  },
+  {
+    "revision": "06112d8fcaf34ba701f7",
+    "url": "/RockPaperScissors/static/js/runtime-main.8180d477.js"
+  }
+]);
